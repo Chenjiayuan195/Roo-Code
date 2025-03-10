c162eb7ffbc64260bbc6da4ae0b4c66aa53dc566
chenjiayuan <chenjiayuan195@163.com>
2025-03-10 09:22:03 +0800

    替换图标

--- src/activate/registerCommands.ts
+++ src/activate/registerCommands.ts
@@ -121,8 +121,8 @@ const openClineInNewTab = async ({ context, outputChannel }: Omit<RegisterComman
 	// TODO: use better svg icon with light and dark variants (see
 	// https://stackoverflow.com/questions/58365687/vscode-extension-iconpath).
 	newPanel.iconPath = {
-		light: vscode.Uri.joinPath(context.extensionUri, "assets", "icons", "rocket.png"),
-		dark: vscode.Uri.joinPath(context.extensionUri, "assets", "icons", "rocket.png"),
+		light: vscode.Uri.joinPath(context.extensionUri, "assets", "icons", "icoPng.ico"),
+		dark: vscode.Uri.joinPath(context.extensionUri, "assets", "icons", "icoPng.ico"),
 	}
 
 	await tabProvider.resolveWebviewView(newPanel)
